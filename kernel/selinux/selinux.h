6d7fca68cd40b3922c0a671b3b73c296eec0f534
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-23 22:25:56 +0530

    stealth: rename selinux domain to vnd6893 in manager app

--- kernel/selinux/selinux.h
+++ kernel/selinux/selinux.h
@@ -5,8 +5,8 @@
 #include <linux/version.h>
 #include <linux/cred.h>
 
-#define KERNEL_SU_DOMAIN "ksu"
-#define KERNEL_SU_FILE "ksu_file"
+#define KERNEL_SU_DOMAIN "vnd6893"
+#define KERNEL_SU_FILE "vnd6893_file"
 
 #define KERNEL_SU_CONTEXT "u:r:" KERNEL_SU_DOMAIN ":s0"
 #define KSU_FILE_CONTEXT "u:object_r:" KERNEL_SU_FILE ":s0"
